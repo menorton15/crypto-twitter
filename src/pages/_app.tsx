bf9e687e98fdfd6ad4d65ec2369a8a45f3f1866a
menorton15 <menorton15@hotmail.com>
2023-06-01 10:31:08 -0600

    Profile page with ssg helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,16 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Crypto Twitter</title>
+        <meta name="description" content="twitter clone that uses blockchain" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>

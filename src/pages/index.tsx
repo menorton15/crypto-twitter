965c81bb842af1966f3c57ed5e212a56b2ab9d53
menorton15 <menorton15@hotmail.com>
2023-06-01 07:40:03 -0600

    New files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -65,7 +66,7 @@ const CreatePostWizard = () => {
           if (e.key === "Enter") {
             e.preventDefault();
             if (input !== "") {
-              mutate({ content: input})
+              mutate({ content: input });
             }
           }
         }}
@@ -100,10 +101,15 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span className="font-bold">{`@${author.username}`}</span>·
-          <span className="font-thin">{`${dayjs(
-            post.createdAt
-          ).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span className="font-bold">{`@${author.username}`}</span>
+          </Link>
+          ·
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{`${dayjs(
+              post.createdAt
+            ).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>

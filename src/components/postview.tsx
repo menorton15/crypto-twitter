b1cd14e0909e71eeeb1a56846abd4743bb5dce29
menorton15 <menorton15@hotmail.com>
2023-06-02 08:27:05 -0600

    Update text

--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -34,7 +34,7 @@ export const PostView = (props: PostWithUser) => {
             ).fromNow()}`}</span>
           </Link>
         </div>
-        <span className="text-2xl">{post.content}</span>
+        <span>{post.content}</span>
       </div>
     </div>
   );
